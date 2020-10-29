f019f5dfc17dc68dc60a39bf80a431dc5dcc00bd
Psy-Fer <fergo1988@gmail.com>
2020-10-29 15:11:32 +1100

    Update detection of stop codons and added -l to bcftools csq

--- scripts/hive.sh
+++ scripts/hive.sh
@@ -15,6 +15,14 @@ module load evaben7/gcc/8.2.0
 module load evaben7/samtools/1.9/gcc-8.2.0
 module load evaben7/bcftools/1.9/gcc-8.2.0
 module load evaben7/htslib/1.9/gcc-8.2.0
+module load shacar/java/jdk-11.0.2
+module load jamfer/jvarkit/2b40bcc
+module load briglo/picard/2.9.4
+
+
+# samtools view -h barcode09_fwdRev.Q10.8500.srt.bam | sam2tsv.jar -r ../../ref/HXB2_trimmed.fasta | awk '($8==5435) && ($6=="T")' > reads_with_T_5435.tsv
+
+
 
 source /home/jamfer/work/venv363/bin/activate
 
@@ -23,9 +31,11 @@ WORK_DIR=$2
 CALLS=$3
 FASTQS=$4
 REF=/directflow/KCCGGenometechTemp/projects/jamfer/HIVE/ref/HXB2_trimmed.fasta
-GFF=/directflow/KCCGGenometechTemp/projects/jamfer/HIVE/ref/HXB2_AA_090220.gff
+GFF=/directflow/KCCGGenometechTemp/projects/jamfer/HIVE/ref/HXB2_AA_17Jul20.gff
 echo -e "[SGE - $(date +"%T")]\tHive-seq starting"
 
+echo -e "[SGE - $(date +"%T")]\tBuilding .dict file"
+java -jar /share/ClusterShare/software/contrib/briglo/picard/build/libs/picard.jar CreateSequenceDictionary R=${REF}
 
 echo -e "[SGE - $(date +"%T")]\tBAMS: ${BAMS}"
 echo -e "[SGE - $(date +"%T")]\tWORK_DIR: ${WORK_DIR}"
@@ -94,7 +104,7 @@ bcftools mpileup -Ov -f ${REF} ${STEM}_fwdRev.Q10.8500.srt.bam > ${STEM}_raw.vcf
 
 bcftools call -v -Ov -m  ${STEM}_raw.vcf -o ${STEM}_raw.calls.vcf
 
-bcftools csq -pa -f ${REF} -g ${GFF} ${STEM}_raw.calls.vcf -Ov -o ${STEM}_variants.csq.vcf
+bcftools csq -l -pa -f ${REF} -g ${GFF} ${STEM}_raw.calls.vcf -Ov -o ${STEM}_variants.csq.vcf
 
 CALLS_TOT=$(grep ^# -v ${STEM}_variants.csq.vcf -c)
 echo -e "[SGE - $(date +"%T")]\tNumber of Calls: ${CALLS_TOT}"
@@ -110,8 +120,17 @@ echo -e "[SGE - $(date +"%T")]\tNumber of Calls: ${CALLS_TOT}"
 #bedops --element-of 1 <(bam2bed < reads.bam) <(vcf2bed < variants.vcf) > overlapping_reads.bed
 
 echo -e "[SGE - $(date +"%T")]\tFiltering stops, might take a while..."
-grep stop_gained ${STEM}_variants.csq.vcf | python3 ${WORK_DIR}/scripts/filter_stops.py ${STEM}_fwdRev.Q10.8500.srt.bam ${STEM}_fwdRev.Q10.8500.fastq  ${STEM}_with_stop.fastq > ${STEM}_no_stop.fastq
+grep stop_gained ${STEM}_variants.csq.vcf
 
+for POS in $(grep stop_gained ${STEM}_variants.csq.vcf | awk '{print $2}'); do BASE=$(grep ${POS} ${STEM}_variants.csq.vcf | awk '{print $5}'); samtools view -h ${STEM}_fwdRev.Q10.8500.srt.bam | sam2tsv.jar -r ${REF} | awk -v a=${POS} -v b=${BASE} '($8==a) && ($6==b)'; done > ${STEM}_reads_to_filter_1.tsv
+python3 ${WORK_DIR}/scripts/filter_stops.py -f ${STEM}_fwdRev.Q10.8500.fastq -t ${STEM}_reads_to_filter_1.tsv -w ${STEM}_with_stop_1.fastq -n ${STEM}_no_stop_1.fastq
+
+POS=''
+BASE=''
+
+# grep stop_gained ${STEM}_variants.csq.vcf | python3 ${WORK_DIR}/scripts/filter_stops.py ${STEM}_fwdRev.Q10.8500.srt.bam ${STEM}_fwdRev.Q10.8500.fastq  ${STEM}_with_stop.fastq > ${STEM}_no_stop.fastq
+# ${WORK_DIR}/scripts/filter_stops.sh ${WORK_DIR} 1 ${STEM} ${REF} ${STEM}_fwdRev.Q10.8500.srt.bam ${STEM}_variants.csq.vcf ${STEM}_fwdRev.Q10.8500.fastq
+# ${WORK_DIR}/scripts/filter_stops.sh ${WORK_DIR} 1 ${STEM} ${REF} ${STEM}_fwdRev.Q10.8500.srt.bam ${STEM}_variants.csq.vcf ${STEM}_fwdRev.Q10.8500.fastq
 
 # ------------------------------------------------------------------------------------
 
@@ -119,13 +138,13 @@ grep stop_gained ${STEM}_variants.csq.vcf | python3 ${WORK_DIR}/scripts/filter_s
 echo -e "[SGE - $(date +"%T")]\tROUND TWO - Finding Patient specific reference"
 # ------------------------------------------------------------------------------------
 
-minimap2 -cx map-ont -t 8 -k15 ${REF} ${STEM}_no_stop.fastq > ${STEM}_no_stop.paf
-minimap2 -ax map-ont -k15 -t 8 ${REF} ${STEM}_no_stop.fastq | samtools view -Sb - | samtools sort -o ${STEM}_no_stop.srt.bam -
+minimap2 -cx map-ont -t 8 -k15 ${REF} ${STEM}_no_stop_1.fastq > ${STEM}_no_stop_1.paf
+minimap2 -ax map-ont -k15 -t 8 ${REF} ${STEM}_no_stop_1.fastq | samtools view -Sb - | samtools sort -o ${STEM}_no_stop_1.srt.bam -
 
 
-BAM2=${STEM}_no_stop.srt.bam
+BAM2=${STEM}_no_stop_1.srt.bam
 samtools index ${BAM2}
-PAF2=${STEM}_no_stop.paf
+PAF2=${STEM}_no_stop_1.paf
 
 # samtools view ${BAM2} Human:60-80 | cut -f1 | sort -u > ${STEM}_FwdReads_2.txt
 #
@@ -173,7 +192,7 @@ bcftools mpileup -Ov -f ${REF} ${BAM2} > ${STEM}_raw_2.vcf
 
 bcftools call -v -Ov -m  ${STEM}_raw_2.vcf -o ${STEM}_raw_2.calls.vcf
 
-bcftools csq -pa -f ${REF} -g ${GFF} ${STEM}_raw_2.calls.vcf -Ov -o ${STEM}_variants_2.csq.vcf
+bcftools csq -l -pa -f ${REF} -g ${GFF} ${STEM}_raw_2.calls.vcf -Ov -o ${STEM}_variants_2.csq.vcf
 
 CALLS_TOT=$(grep ^# -v ${STEM}_variants_2.csq.vcf -c)
 echo -e "[SGE - $(date +"%T")]\tNumber of Calls: ${CALLS_TOT}"
@@ -183,8 +202,17 @@ echo -e "[SGE - $(date +"%T")]\tFiltering stops again, might take a while..."
 # This checks for any stops after stops were filtered (there should be none)
 
 # grep stop_gained ${STEM}_variants_2.csq.vcf | python3 ${WORK_DIR}/scripts/filter_stops.py ${STEM}_fwdRev.Q10.8500_2.srt.bam ${STEM}_no_stop.fastq  ${STEM}_with_stop_2.fastq > ${STEM}_no_stop_2.fastq
-grep stop_gained ${STEM}_variants_2.csq.vcf | python3 ${WORK_DIR}/scripts/filter_stops.py ${BAM2} ${STEM}_no_stop.fastq  ${STEM}_with_stop_2.fastq > ${STEM}_no_stop_2.fastq
-STOP_READ_COUNT=$(grep runid ${STEM}_with_stop_2.fastq -c)
+# grep stop_gained ${STEM}_variants_2.csq.vcf | python3 ${WORK_DIR}/scripts/filter_stops.py ${BAM2} ${STEM}_no_stop_1.fastq  ${STEM}_with_stop_2.fastq > ${STEM}_no_stop_2.fastq
+grep stop_gained ${STEM}_variants_2.csq.vcf
+
+for POS in $(grep stop_gained ${STEM}_variants_2.csq.vcf | awk '{print $2}'); do BASE=$(grep ${POS} ${STEM}_variants_2.csq.vcf | awk '{print $5}'); samtools view -h ${BAM2} | sam2tsv.jar -r ${REF} | awk -v a=${POS} -v b=${BASE} '($8==a) && ($6==b)'; done > ${STEM}_reads_to_filter_2.tsv
+python3 ${WORK_DIR}/scripts/filter_stops.py -f ${STEM}_no_stop_1.fastq -t ${STEM}_reads_to_filter_2.tsv -w ${STEM}_with_stop_2.fastq -n ${STEM}_no_stop_2.fastq
+
+POS=''
+BASE=''
+# ${WORK_DIR}/scripts/filter_stops.sh ${WORK_DIR} 2 ${STEM} ${REF} ${BAM2} ${STEM}_variants_2.csq.vcf ${STEM}_no_stop_1.fastq
+
+STOP_READ_COUNT=$(grep ^@ ${STEM}_with_stop_2.fastq -c)
 
 # Should be zero
 echo -e "[SGE - $(date +"%T")]\tNumber of stop reads (should be zero): ${STOP_READ_COUNT}"
@@ -252,22 +280,32 @@ PAF3=${STEM}_pt_ref_mapped.paf
 # minimap2 -ax map-ont -k15 -t 8 ${REF2} ${STEM}_fwdRev.Q10.8500_3.fastq | samtools view -Sb - | samtools sort -o ${STEM}_fwdRev.Q10.8500_3.srt.bam -
 #
 #
-# samtools index ${STEM}_fwdRev.Q10.8500_3.srt.bam
-#
-# samtools faidx ${REF2}
+samtools index ${BAM3}
 
-# bcftools mpileup -Ov -f ${REF2} ${STEM}_fwdRev.Q10.8500_3.srt.bam > ${STEM}_raw_3.vcf
-#
-# bcftools call -v -Ov -m  ${STEM}_raw_3.vcf -o ${STEM}_raw_3.calls.vcf
-#
-# bcftools csq -pa -f ${REF2} -g ${GFF} ${STEM}_raw_3.calls.vcf -Ov -o ${STEM}_variants_3.csq.vcf
-#
-# CALLS_TOT=$(grep ^# -v ${STEM}_variants_3.csq.vcf -c)
-# echo -e "[SGE - $(date +"%T")]\tNumber of Calls: ${CALLS_TOT}"
-#
-# echo -e "[SGE - $(date +"%T")]\tFiltering stops again, might take a while..."
-# grep stop_gained ${STEM}_variants_3.csq.vcf | python3 ${WORK_DIR}/scripts/filter_stops.py ${STEM}_fwdRev.Q10.8500_3.srt.bam ${FASTQS}/${BASE%.srt*}.fastq  ${STEM}_with_stop_3.fastq > ${STEM}_no_stop_3.fastq
+samtools faidx ${REF2}
 
+bcftools mpileup -Ov -f ${REF2} ${BAM3} > ${STEM}_raw_3.vcf
+
+bcftools call -v -Ov -m  ${STEM}_raw_3.vcf -o ${STEM}_raw_3.calls.vcf
+
+bcftools csq -l -pa -f ${REF2} -g ${GFF} ${STEM}_raw_3.calls.vcf -Ov -o ${STEM}_variants_3.csq.vcf
+
+CALLS_TOT=$(grep ^# -v ${STEM}_variants_3.csq.vcf -c)
+echo -e "[SGE - $(date +"%T")]\tNumber of Calls: ${CALLS_TOT}"
+
+echo -e "[SGE - $(date +"%T")]\tFiltering stops again, might take a while..."
+# grep stop_gained ${STEM}_variants_3.csq.vcf | python3 ${WORK_DIR}/scripts/filter_stops.py ${BAM3} ${FASTQ3} ${STEM}_with_stop_3.fastq > ${STEM}_no_stop_3.fastq
+
+echo -e "[SGE - $(date +"%T")]\tBuilding .dict file"
+java -jar /share/ClusterShare/software/contrib/briglo/picard/build/libs/picard.jar CreateSequenceDictionary R=${REF2}
+
+grep stop_gained ${STEM}_variants_3.csq.vcf
+
+for POS in $(grep stop_gained ${STEM}_variants_3.csq.vcf | awk '{print $2}'); do BASE=$(grep ${POS} ${STEM}_variants_3.csq.vcf | awk '{print $5}'); samtools view -h ${BAM3} | sam2tsv.jar -r ${REF2} | awk -v a=${POS} -v b=${BASE} '($8==a) && ($6==b)'; done > ${STEM}_reads_to_filter_3.tsv
+python3 ${WORK_DIR}/scripts/filter_stops.py -f ${FASTQ3} -t ${STEM}_reads_to_filter_3.tsv -w ${STEM}_with_stop_3.fastq -n ${STEM}_no_stop_3.fastq
+POS=''
+BASE=''
+# ${WORK_DIR}/scripts/filter_stops.sh ${WORK_DIR} 3 ${STEM} ${REF2} ${BAM3} ${STEM}_variants_3.csq.vcf ${FASTQ3}
 
 echo -e "[SGE - $(date +"%T")]\tDONE"
 
